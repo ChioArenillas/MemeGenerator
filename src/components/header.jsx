0c62435b86c7db75b76af98ecd6e5f4d4f9ee315
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-03-11 09:56:04 +0100

    new

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
         <header className="header">
-            <img src="../assets/Trollface.png"/>
+            <img src="../../public/Trollface.png"/>
             <h1>Meme Generator</h1>
         </header>
   )
