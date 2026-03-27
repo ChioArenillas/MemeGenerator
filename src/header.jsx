266faccff3ad21d2066e334458d08d9f3a6d95b4
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-03-27 12:10:54 +0100

    new icon

--- src/header.jsx
+++ src/header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import trollFace from "../public/trollFace"
+import trollFace from "../public/trollFace.png"
 
 export default function Header() {
   return (
         <header className="header">
+          <img src={trollFace}></img>
             <h1>Meme Generator</h1>
         </header>
   )
